ce13bfb866430ecabeea0be8450ded2038390bb6
Ivo Kund <ivo@pactum.com>
2024-12-02 23:31:46 +0200

    Day 1, part 2

--- day02/main.py
+++ day02/main.py
@@ -23,42 +23,28 @@ def is_safe(report):
             and max(abs_deltas) <= 3
             and (all(delta > 0 for delta in deltas) or (all(delta < 0 for delta in deltas))))
 
-def get_bad_level_count(report):
-    print("REPORT: ", report)
-    number_of_bad_levels = 0
-    sign = report[0] > report[1]
-    print("decreasing: ", sign)
-    for i in range(len(report) - 1):
-        delta = abs(report[i] - report[i+1])
-        print("i: ", i, "delta: ", delta)
-        bad_delta = delta > 3 or delta < 1
-        # check if the sign is the same
-        bad_sign = (report[i] > report[i+1]) is not sign
-        print("bad_delta", bad_delta)
-        print("bad_sign", bad_sign)
-        if bad_delta or bad_sign:
-            number_of_bad_levels += 1
-    print("SAFE" if number_of_bad_levels == 0 else "UNSAFE")
-    print("bad levels: ", number_of_bad_levels)
-    print("")
-    return number_of_bad_levels
 
 def part1(text):
     reports = parse(text)
-    # print(get_bad_level_count(reports[0]))
-    safe_count = filter(lambda x: get_bad_level_count(x) == 0, reports)
+    safe_count = filter(is_safe, reports)
     return len(list(safe_count))
+
 
 def part2(text):
     reports = parse(text)
-    # print(get_bad_level_count(reports[0]))
-    safe_count = filter(lambda x:
-                        get_bad_level_count(x) <= 1
-                        , reports)
+
+    def is_safe_with_removing_one(report):
+        for i in range(len(report)):
+            new_report = report[:i] + report[i+1:]
+            if is_safe(new_report):
+                return True
+        return False
+
+    safe_count = filter(is_safe_with_removing_one, reports)
     return len(list(safe_count))
 
 
 print("Part 1 test: ", part1(test_data))
 print("Part 1 real: ", part1(real_data))
-# print("Part 2 test: ", part2(test_data))
-# print("Part 2 real: ", part2(real_data))
+print("Part 2 test: ", part2(test_data))
+print("Part 2 real: ", part2(real_data))
