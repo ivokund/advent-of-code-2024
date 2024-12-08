dac1fbb28cdc33fb831275330609d8556d0f755a
Ivo Kund <ivo@pactum.com>
2024-12-08 22:09:06 +0200

    Day 8, part 1

--- day08/main.py
+++ day08/main.py
@@ -1,4 +1,15 @@
-test_data = """............
+test_data = """T.........
+...T......
+.T........
+..........
+..........
+..........
+..........
+..........
+..........
+.........."""
+
+test_data2 = """............
 ........0...
 .....0......
 .......0....
@@ -24,32 +35,6 @@ def parse(text):
                 coords[char].append((x, y))
     return coords, len(lines[0]), len(lines)
 
-    # return Map(coords, len(lines[0]), len(lines))
-
-
-class Map:
-    def __init__(self, coords, width, height):
-        self.coords = coords
-        self.width = width
-        self.height = height
-
-
-    def coord_in_map(self, coords):
-        x, y = coords
-        return 0 <= x < self.width and 0 <= y < self.height
-
-    # def __str__(self):
-    #     result = ""
-    #
-    #     for y in range(self.height):
-    #         for x in range(self.width):
-    #             if (x, y) in self.obstacles:
-    #                 result += "#"
-    #             else:
-    #                 result += "."
-    #         result += "\n"
-    #     return result
-
 
 def run(text, use_resonance=False):
     coords, width, height = parse(text)
@@ -58,25 +43,35 @@ def run(text, use_resonance=False):
         x, y = coords
         return 0 <= x < width and 0 <= y < height
 
+    all_vectors = []  # start pos, vector
     antinodes = []
     for key in coords:
-        # print("For ", key)
+        print("For ", key)
         for coord1 in coords[key]:
             for coord2 in coords[key]:
                 if coord1 != coord2:
-                    # print("Points: ", coord1, coord2)
+                    print("Points: ", coord1, coord2)
                     dx, dy = (coord2[0] - coord1[0]), (coord2[1] - coord1[1])
-                    antinode = (coord1[0] + 2 * dx), (coord1[1] + 2 * dy)
+                    antinode = (coord1[0] - dx), (coord1[1] - dy)
                     if antinode not in antinodes and is_in_map(antinode):
                         antinodes.append(antinode)
-                    # print("Antinode 1: ", antinode)
+                        # extra_coords.append(coord1)
+                        # extra_coords.append(coord2)
+                    if use_resonance:
+                        while is_in_map(antinode):
+                            antinode = (antinode[0] + dx), (antinode[1] + dy)
+                            print("Antinode: ", antinode)
 
-    # print(coords)
-    # print(antinodes)
+                            if antinode not in antinodes and is_in_map(antinode):
+                                antinodes.append(antinode)
+                                # extra_coords.append(coord1)
+                                # extra_coords.append(coord2)
+
+    print(antinodes)
     return len(antinodes)
 
-print("Part 1 test: ", run(test_data))
+
+print("Part 1 test: ", run(test_data2))
 print("Part 1 real: ", run(real_data))
-# print("Part 2 test: ", part2(test_data))
+# print("Part 2 test: ", run(test_data2, True))
 # print("Part 2 real: ", part2(real_data))
-#  wrong: 1664
