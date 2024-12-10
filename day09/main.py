8ec43cca495c902f88263f9ea3a236438a98f48c
Ivo Kund <ivo@pactum.com>
2024-12-10 22:26:05 +0200

    Day 9, part 1

--- day09/main.py
+++ day09/main.py
@@ -1,9 +1,4 @@
-# test_data = """010"""
-# test_data = """12345"""
-test_data = """2333133121414131402""" # 1928
-# test_data = """10101010101010101010101""" # 503
-# test_data = """111111111111111111111""" # 290
-# test_data = """1010101010101010101010""" # 385
+import unittest
 
 real_data = open("input.txt").read()
 
@@ -14,12 +9,8 @@ def parse(text):
     for idx, char in enumerate(text):
         is_file = idx % 2 == 0
         block_count = int(char)
-        if is_file and block_count > 0: ## ???
-            result.append({
-                "block_count": block_count,
-                "is_file": True,
-                "file_label": file_id
-            })
+        if is_file and block_count > 0:
+            result.append({"block_count": block_count, "is_file": True, "file_label": file_id})
         else:
             result.extend([{"is_file": False}] * block_count)
 
@@ -28,48 +19,39 @@ def parse(text):
     return result
 
 
+def get_sum(blocks):
+    result = 0
+    idx = 0
+    for element in blocks:
+        if element['is_file']:
+            for _ in range(element['block_count']):
+                # print("-- adding sum ({} x {}) = {}".format(element['file_label'], idx, element['file_label'] * idx))
+                result += element['file_label'] * idx
+                idx += 1
+    return result
+
+
+def draw(file):
+    result = ""
+    for element in file:
+        if element['is_file']:
+            result += "(" + str(element['file_label']) * element['block_count'] + ")"
+        else:
+            result += "."
+    print(result)
+    print("")
+
+
 def part1(text):
     blocks = parse(text)
-
-    def get_sum(blocks):
-        result = 0
-        idx = 0
-        for element in blocks:
-            if element['is_file']:
-                for _ in range(element['block_count']):
-                    # print("-- adding sum ({} x {}) = {}".format(element['file_label'], idx, element['file_label'] * idx))
-                    result += element['file_label'] * idx
-                    idx += 1
-        return result
-
-    def draw(file):
-        result = ""
-        for element in file:
-            if element['is_file']:
-                result += "(" + str(element['file_label']) * element['block_count'] + ")"
-            else:
-                result += "."
-        print(result)
-        print("")
-    #
-    # print(blocks)
-    # draw(blocks)
-    # print("sum", get_sum(blocks))
-    # exit(0)
-
-
     while True:
-        # print("== LOOP ==")
-        # print(blocks)
-        # print(" start:")
-        # draw(blocks)
         file_indices = [i for i, block in enumerate(blocks) if block["is_file"]]
         last_file_pos = file_indices[-1] if file_indices else None
         # print("- last_file_pos", last_file_pos)
         first_free_pos = next((idx for idx, block in enumerate(blocks) if not block["is_file"]), None)
         # print("- first_free_pos", first_free_pos)
         if first_free_pos is None or first_free_pos >= last_file_pos:
-            # print(" - breaking")
+            # print("- breaking")
             break
 
         # create number node to add to beginning
@@ -95,15 +77,18 @@ def part1(text):
         # draw(blocks)
 
     # print("end")
-    draw(blocks)
+    # draw(blocks)
     return get_sum(blocks)
 
 
-print("Part 1 test: ", part1(test_data))
-print("Part 1 real: ", part1(real_data))
-# print("Part 2 test: ", part2(test_data))
-# print("Part 2 real: ", part2(real_data))
+class Test(unittest.TestCase):
+    def test_part1_test(self):
+        self.assertEqual(part1("12345"), 60)
+        self.assertEqual(part1("2333133121414131402"), 1928)
+        self.assertEqual(part1("111111111111111111111"), 290)
+        self.assertEqual(part1("10101010101010101010101"), 506)
+        self.assertEqual(part1("1010101010101010101010"), 385)
 
-# wrong 90095094087
-# wrong 85447704075, 85535077891, 85535077891
-# wrong 2735004463647223305301935318131716490526069
+    def test_part1_real(self):
+        self.assertEqual(part1(real_data), 0)
+
