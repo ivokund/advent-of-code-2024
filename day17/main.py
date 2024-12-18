42d1a73e79ae11a3f4669b9ffede2394e1618c61
Ivo Kund <ivo@pactum.com>
2024-12-18 21:33:16 +0200

    Day 17, part 2

--- day17/main.py
+++ day17/main.py
@@ -1,5 +1,4 @@
 from collections import namedtuple
-from typing import Set
 
 test_data = """5,4
 4,2
@@ -37,7 +36,7 @@ def draw(obstacles, grid_size, highlight_nodes):
     for i, coord in enumerate(obstacles):
         grid[coord.y][coord.x] = '#'
     for i, coord in enumerate(highlight_nodes):
-        grid[coord.y][coord.x] = 'O'
+        grid[coord.y][coord.x] = 'o'
     for row in grid:
         print("".join(row))
 
@@ -45,6 +44,7 @@ def draw(obstacles, grid_size, highlight_nodes):
 def find_shortest_path_djikstra(start, end, obstacles, grid_size):
     visited = set()
     unvisited = set()
+    came_from = {}
     for i in range(grid_size):
         for j in range(grid_size):
             unvisited.add(Point(i, j))
@@ -62,16 +62,47 @@ def find_shortest_path_djikstra(start, end, obstacles, grid_size):
                 continue
             new_distance = distances[current] + 1
             if new_distance < distances[new_point]:
+                came_from[new_point] = current
                 distances[new_point] = new_distance
-    return distances[end]
+
+    if end not in came_from:
+        return None, None
+    path = []
+    current = end
+    while current != start:
+        path.append(current)
+        current = came_from[current]
+    return distances[end], path
+
+
+def parse(text, until):
+    return [Point(*(int(num) for num in line.split(","))) for line in text.split('\n')[:until]]
 
 
 def part1(text, grid_size, byte_count):
-    coords = [Point(*(int(num) for num in line.split(","))) for line in text.split('\n')[:byte_count]]
-    path = find_shortest_path_djikstra(Point(0, 0), Point(grid_size-1, grid_size-1), coords, grid_size)
-    return path
+    coords = parse(text, byte_count)
+    count, _ = find_shortest_path_djikstra(
+        Point(0, 0),
+        Point(grid_size-1, grid_size-1),
+        coords,
+        grid_size
+    )
+    return count
+
+
+def part2(text, grid_size):
+    all_byte_coords = parse(text, 5000)
+    shortest_path = None
+    start, end = Point(0, 0), Point(grid_size-1, grid_size-1)
+    for idx, coord in enumerate(all_byte_coords):
+        obstacles = all_byte_coords[:idx+1]
+        if shortest_path is None or coord in shortest_path:
+            _, shortest_path = find_shortest_path_djikstra(start, end, obstacles, grid_size)
+        if shortest_path is None:
+            return f"{coord.x},{coord.y}"
 
 
 print("Part 1 test: ", part1(test_data, 7, 12))
 print("Part 1 real: ", part1(real_data, 71, 1024))
-
+print("Part 2 test: ", part2(test_data, 7))
+print("Part 2 real: ", part2(real_data, 71))
