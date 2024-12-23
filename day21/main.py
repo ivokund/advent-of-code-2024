896c1cc1d854f4149e446403ccb59e660525526c
Ivo Kund <ivo@pactum.com>
2024-12-23 19:14:34 +0200

    Dai 21, part 1

--- day21/main.py
+++ day21/main.py
@@ -1,6 +1,6 @@
 import unittest
 from collections import namedtuple
-from typing import Tuple, List
+from typing import Text
 
 test_data = """029A
 980A
@@ -21,6 +21,13 @@ MoveLabels = {
     (-1, 0): '<',
     (0, 1): 'v',
     (0, -1): '^',
+}
+
+MoveDeltas = {
+    '>': (1, 0),
+    '<': (-1, 0),
+    'v': (0, 1),
+    '^': (0, -1),
 }
 
 # +---+---+---+
@@ -46,28 +53,12 @@ NumericKeys = {
     'A': Key(2, 3),
     'BLANK': Key(0, 3),
 }
-# ^A^^<<A>>AvvvA
-#   < ?
-# <A>A <AAv<AA>>^AvAA^Av<AAA^>A
-#      v<<A ???
-# <v<A>>^AvA^A <vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A -- correct
-# v<<A>>^AvA^A v<<A>>^AAv<A<A>>^AAvAA^<A>Av<A^>AA<A>Av<A<A>>^AAA<Av>A^A
 #     +---+---+
 #     | ^ | A |
 # +---+---+---+
 # | < | v | > |
 # +---+---+---+
 
-
-
-# 375A: ^A^^<<Av>Avv>A length: 14
-#       ^A^^<<A>vAvv>A
-#
-# 375A: <A>A<AAv<AA>>^Av<A>A^Av<AA>A^A length: 30
-#       <A>A<AAv<AA>>^AvA<A^>Av<AA>A^A
-#
-# 375A: v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A <A>>^AvA^A<A>Av<A<A>>^AAvA^A<A>A length: 74
-#       v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A ^>Av<<A>>^A<Av>A^Av<A<A>>^AAvA^A<A>A
 DirectionalKeys = {
     '<': Key(0, 1),
     'v': Key(1, 1),
@@ -77,95 +68,72 @@ DirectionalKeys = {
     'BLANK': Key(0, 0),
 }
 class Keypad:
-    def __init__(self, keys, start_key='A'):
+    def __init__(self, keys):
         self.keys = keys
         for val, coords in keys.items():
-            if val == start_key:
-                print(f'  Setting start to {coords}')
-                self.position = coords
-            elif val == 'BLANK':
-                print(f'  Setting BLANK to {coords}')
+            if val == 'BLANK':
+                # print(f'  Setting BLANK to {coords}')
                 self.blank_pos = coords
+            elif val == 'A':
+                # print(f'  Setting A to {coords}')
+                self.a_pos = coords
 
-    def calculate_steps_to(self, to_key):
-        to_coords = self.keys[to_key]
-        dx = to_coords.x - self.position.x
-        dy = to_coords.y - self.position.y
+    # def get_paths_str(self, start, end):
+    #     paths = self.get_all_paths(start, end)
+    #     return ["".join([MoveLabels[move] for move in path]) for path in paths]
 
-        my_pos = self.position
-        moves = []
+    def get_all_paths(self, start, end):
+        from_coord = self.keys[start]
+        to_coords = self.keys[end]
+        dx = to_coords.x - from_coord.x
+        dy = to_coords.y - from_coord.y
 
         step_x = dx // abs(dx) if dx != 0 else 0
         step_y = dy // abs(dy) if dy != 0 else 0
 
-        print(f"=== moving from {my_pos} to {to_coords} with steps {step_x}, {step_y}")
+        move_x = (step_x, 0)
+        move_y = (0, step_y)
 
-        def move_x_axis():
-            nonlocal my_pos
-            if my_pos == to_coords or dx == 0:
-                return
-            while True:
-                new_pos = Key(my_pos.x + step_x, my_pos.y)
-                if new_pos == self.blank_pos:
-                    break
-                my_pos = new_pos
-                moves.append((step_x, 0))
-                if new_pos.x == to_coords.x:
-                    break
-
-        def move_y_axis():
-            nonlocal my_pos
-            if my_pos == to_coords or dy == 0:
-                return
-            while True:
-                new_pos = Key(my_pos.x, my_pos.y + step_y)
-                if new_pos == self.blank_pos:
-                    break
-                my_pos = new_pos
-                moves.append((0, step_y))
-                if new_pos.y == to_coords.y:
-                    break
-
-        x_first = my_pos.x == self.blank_pos.x
-        print(f"  - x first: {x_first}")
-
-        for _ in [1, 2]:
-            if x_first:
-                move_x_axis()
-                move_y_axis()
+        final_paths = set([])
+        paths_queue = [([], from_coord)]
+        while paths_queue:
+            path, my_pos = paths_queue.pop(0)
+            if my_pos == to_coords:
+                final_paths.add(''.join([MoveLabels[move] for move in path]))
+            elif my_pos == self.blank_pos:
+                continue
             else:
-                move_y_axis()
-                move_x_axis()
+                if my_pos.x != to_coords.x:
+                    new_pos = Key(my_pos.x + step_x, my_pos.y)
+                    paths_queue.append((path + [move_x], new_pos))
+                if my_pos.y != to_coords.y:
+                    new_pos = Key(my_pos.x, my_pos.y + step_y)
+                    paths_queue.append((path + [move_y], new_pos))
+        # print(f"  Generated paths:")
+        # for idx, path in enumerate(final_paths):
+        #     print(f"  Path {idx+1}: {path}")
+        return final_paths
 
-        return moves
-    
-    def get_print_steps_to(self, to_key):
-        moves = self.calculate_steps_to(to_key)
-        moves_str = [MoveLabels[move] for move in moves] + ['A']
-        return "".join(moves_str)
+    def get_paths_for_key_sequence(self, sequence: Text):
+        # print(f"Getting paths for sequence: {sequence}")
+        paths_to_prepend = {''}
+        start = 'A'
+        for key in list(sequence):
+            # print(f"  - moving from {start} to {key}")
+            paths = self.get_all_paths(start, key)
+            # print(f"  - received paths: {paths}")
+            start = key
+            new_paths = []
+            for path in paths:
+                for prev_path in paths_to_prepend:
+                    new_paths.append(prev_path + path + 'A')
+            paths_to_prepend = new_paths
+            # print(f"  - new paths to prepend: {paths_to_prepend}")
 
-    def move(self, moves: List[Tuple[int, int]]):
-        print(f"    == moving by steps {moves} ==")
-
-        for dx, dy in moves:
-            self.position = Key(self.position.x + dx, self.position.y + dy)
-            print(f"  - moved to {self.position}")
-
-        moves_str = "".join([MoveLabels[move] for move in moves] + ['A'])
-        print(f"  - moves: {moves_str}")
-        return moves_str
-
-    def move_sequence(self, sequence):
-        all_moves = ""
-        for key in sequence:
-            moves = self.calculate_steps_to(key)
-            all_moves += self.move(moves)
-        print(f"  - final position: {self.position}")
-        return all_moves
+        return paths_to_prepend
 
 
 class Day21(unittest.TestCase):
-    @unittest.skip
     def test_numeric(self):
         # +---+---+---+
         # | 7 | 8 | 9 |
@@ -177,19 +145,31 @@ class Day21(unittest.TestCase):
         #     | 0 | A |
         #     +---+---+
         pad = Keypad(NumericKeys)
-        self.assertEqual('<A', pad.get_print_steps_to(0))
-        self.assertEqual('^A', pad.get_print_steps_to(3))
-        self.assertEqual('^^^A', pad.get_print_steps_to(9))
-        self.assertEqual('^^^<<A', pad.get_print_steps_to(7))
-        self.assertEqual('^<<A', pad.get_print_steps_to(1))
+        self.assertEqual({'<'}, pad.get_all_paths('A', 0))
+        self.assertEqual({'^'}, pad.get_all_paths('A', 3))
+        self.assertEqual({'^^^'}, pad.get_all_paths('A', 9))
+        self.assertEqual({'<^<^^',
+                          '<^^<^',
+                          '<^^^<',
+                          '^<<^^',
+                          '^<^<^',
+                          '^<^^<',
+                          '^^<<^',
+                          '^^<^<',
+                          '^^^<<'}, pad.get_all_paths('A', 7))
+        self.assertEqual({'<^<', '^<<'}, pad.get_all_paths('A', 1))
 
-        pad = Keypad(NumericKeys, 7)
-        self.assertEqual('>>vvvA', pad.get_print_steps_to('A'))
+        pad = Keypad(NumericKeys)
+        self.assertEqual({'>>vvv',
+                          '>v>vv',
+                          '>vv>v',
+                          '>vvv>',
+                          'v>>vv',
+                          'v>v>v',
+                          'v>vv>',
+                          'vv>>v',
+                          'vv>v>'}, pad.get_all_paths(7, 'A'))
 
-        pad = Keypad(NumericKeys, 3)
-        self.assertEqual('asdasd', pad.get_print_steps_to(7))
-
-    @unittest.skip
     def test_directional(self):
         #     +---+---+
         #     | ^ | A |
@@ -197,19 +177,18 @@ class Day21(unittest.TestCase):
         # | < | v | > |
         # +---+---+---+
         pad = Keypad(DirectionalKeys)
-        self.assertEqual('vA', pad.get_print_steps_to('>'))
-        self.assertEqual('v<<A', pad.get_print_steps_to('<'))
+        self.assertEqual({'v'}, pad.get_all_paths('A', '>'))
+        self.assertEqual({'<v<', 'v<<'}, pad.get_all_paths('A', '<'))
 
-    @unittest.skip
     def test_move_sequence(self):
-        pad = Keypad(NumericKeys)
-        steps = pad.move_sequence([0, 5])
-        self.assertEqual('<A^^A', steps)
+        pad = Keypad(DirectionalKeys)
+        new_steps = pad.get_paths_for_key_sequence('vA')
+        self.assertEqual({'v<A^>A', 'v<A>^A', '<vA^>A', '<vA>^A'}, new_steps)
 
 
     def test_part1(self):
         self.assertEqual(126384, part1(test_data))
-        # self.assertEqual(111, part1(real_data))
+        self.assertEqual(111, part1(real_data))
 
 
 def part1(text):
@@ -219,34 +198,43 @@ def part1(text):
     directional_pad_1 = Keypad(DirectionalKeys)
     directional_pad_2 = Keypad(DirectionalKeys)
 
-    def get_moves(code):
-        robot_1_moves = numeric_pad.move_sequence(code)
-        print("Robot 1 moves (numeric):", robot_1_moves)
+    def discard_longer_paths(paths):
+        different_lengths = set([])
+        for sequence in paths:
+            different_lengths.add(len(sequence))
+        min_l, max_l = min(list(different_lengths)), max(list(different_lengths))
+        # print(f"  - Got {len(different_lengths)} different lengths: {min_l} - {max_l}")
 
-        robot_2_moves = directional_pad_1.move_sequence(robot_1_moves)
-        print("Robot 2 moves (directional):", robot_2_moves)
+        paths_with_min_length = set([path for path in paths if len(path) == min_l])
+        return paths_with_min_length, min_l
 
-        robot_3_moves = directional_pad_2.move_sequence(robot_2_moves)
-        print("Robot 3 moves (directional):", robot_3_moves)
+    def get_min_sequence_length(code):
+        # print("Code:", code)
+        robot_1_paths = numeric_pad.get_paths_for_key_sequence(code)
+        robot_1_paths_short, r1_min = discard_longer_paths(robot_1_paths)
 
-        return robot_3_moves
+        # print(f"Robot 1 paths, keeping {len(robot_1_paths_short)}/{len(robot_1_paths)} paths, with min len: {r1_min}:")
 
+        robot_2_paths = set()
+        for robot_1_path in robot_1_paths:
+            # print(f"  - {robot_1_path}")
+            robot_2_paths.update(directional_pad_1.get_paths_for_key_sequence(robot_1_path))
 
+        # print("Robot 2 paths:", len(robot_2_paths))
+        robot_2_paths, r2_min = discard_longer_paths(robot_2_paths)
+        # print(f"Robot 2 done, after cleanup: {len(robot_2_paths)} paths, min len: {r2_min}")
 
-    def get_complexity(moves, code):
-        return len(moves), int("".join([str(num) for num in code if num != 'A']))
+        robot_3_paths = set()
+        for robot_2_path in robot_2_paths:
+            # print(f"      -  {robot_2_path}")
+            robot_3_paths.update(directional_pad_2.get_paths_for_key_sequence(robot_2_path))
+        # print("Robot 3 paths:", len(robot_3_paths))
+        robot_3_paths, r3_min = discard_longer_paths(robot_3_paths)
+        # print(f"Robot 3 done, after cleanup: {len(robot_3_paths)} paths, min len: {r3_min}")
 
-    codes = [[3, 7, 5, 'A']]
-    m = get_moves(codes[0])
-    print(get_complexity(m, codes[0]))
+        return r3_min
 
-    complexities = [get_complexity(get_moves(code), code) for code in codes]
-    #
-    return (complexities)
+    def get_complexity(code):
+        return get_min_sequence_length(code) * int("".join([str(num) for num in code if num != 'A']))
 
-
-# print("Part 1 test: ", part1(test_data))
-# print("Part 1 real: ", part1(real_data, 71, 1024))
-# print("Part 2 test: ", part2(test_data, 7))
-# print("Part 2 real: ", part2(real_data, 71))
-# wrong 165808
+    return sum([get_complexity(code) for code in codes])
