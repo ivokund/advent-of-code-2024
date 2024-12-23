82cbbb26b4bd64f7d451ea5a5e30ace7e6928851
Ivo Kund <ivo@pactum.com>
2024-12-23 23:26:38 +0200

    Dai 22, part 2

--- day22/main.py
+++ day22/main.py
@@ -1,8 +1,14 @@
 import unittest
+from collections import defaultdict
 
 test_data = """1
 10
 100
+2024"""
+
+test_data_2 = """1
+2
+3
 2024"""
 
 
@@ -29,12 +35,42 @@ def simulate_to(secret: int, count: int) -> int:
     return secret
 
 
+def get_prices_per_sequence(secret, total_iterations):
+    secrets = [secret]
+    for _ in range(total_iterations-1):
+        secrets.append(evolve(secrets[-1]))
+
+    prices = [secret % 10 for secret in secrets]
+    price_diffs = [None] + [prices[i] - prices[i - 1] for i in range(1, len(prices))]
+
+    price_per_sequence = {}
+    for idx in range(4, len(prices)):
+        sequence_str = ",".join(map(str, price_diffs[idx-3:idx+1]))
+        if sequence_str not in price_per_sequence:
+            price_per_sequence[sequence_str] = prices[idx]
+
+    return price_per_sequence
+
+
 def part1(text):
     numbers = list(map(int, text.split("\n")))
     return sum([simulate_to(num, 2000) for num in numbers])
 
 
-class OperationsTest(unittest.TestCase):
+def part2(text):
+    numbers = list(map(int, text.split("\n")))
+
+    all_prices_per_sequence = defaultdict(int)
+
+    for num in numbers:
+        prices_per_sequence = get_prices_per_sequence(num, 2000)
+        for sequence, price in prices_per_sequence.items():
+            all_prices_per_sequence[sequence] += price
+
+    return max(all_prices_per_sequence.values())
+
+
+class Tests(unittest.TestCase):
     def test_prune(self):
         self.assertEqual(prune(100000000), 16113920)
 
@@ -51,3 +87,10 @@ class OperationsTest(unittest.TestCase):
         self.assertEqual(part1(test_data), 37327623)
         self.assertEqual(part1(real_data), 17163502021)
 
+    def test_get_sequences_per_price(self):
+        prices = get_prices_per_sequence(123, 10)
+        self.assertEqual(prices['-1,-1,0,2'], 6)
+
+    def test_part2(self):
+        self.assertEqual(part2(test_data_2), 23)
+        self.assertEqual(part2(real_data), 1938)
